255e1656900ed08a24275646c726fb2870050161
suhairii <suhairi14000@gmail.com>
2024-03-23 10:04:36 +0700

    hehe

--- P07-Widget/widget_dasar/lib/main.dart
+++ P07-Widget/widget_dasar/lib/main.dart
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'EdgeInsets.all App',
+      title: 'TextAlign App',
       theme: ThemeData(
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
@@ -20,45 +20,65 @@ class MyApp extends StatelessWidget {
 }
 
 class Home extends StatelessWidget {
+  final String teks1 =
+      '''Nilai TextAlign.start pada teks dengan TextDirection.ltr''';
+  final String teks2 =
+      '''Nilai TextAlign.end pada teks dengan TextDirection.ltr''';
+  final String teks3 =
+      '''Nilai TextAlign.start pada teks dengan TextDirection.rtl''';
+  final String teks4 =
+      '''Nilai TextAlign.end pada teks dengan TextDirection.rtl''';
   const Home({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Metode EdgeInsets.all'),
+        title: const Text('Metode Text.textAlign'),
       ),
-      body: Row(
+      body: Column(
         children: <Widget>[
           Container(
-            color: Colors.tealAccent,
             margin: const EdgeInsets.all(10.0),
-            padding: const EdgeInsets.all(3.0),
-            child: ElevatedButton(
-              child: const Text('Button 1'),
-              onPressed: () {},
+            child: Text(
+              teks1,
+              style: const TextStyle(
+                fontSize: 18.0,
+              ),
+              textDirection: TextDirection.ltr,
+              textAlign: TextAlign.start,
+            ),
+          ),
+          Container(
+            margin: const EdgeInsets.all(10.0),
+            child: Text(
+              teks2,
+              style: const TextStyle(
+                fontSize: 18.0,
+              ),
+              textDirection: TextDirection.ltr,
+              textAlign: TextAlign.end,
+            ),
+          ),
+          Container(
+            margin: const EdgeInsets.all(10.0),
+            child: Text(
+              teks3,
+              style: const TextStyle(
+                fontSize: 18.0,
+              ),
+              textDirection: TextDirection.rtl,
+              textAlign: TextAlign.start,
             ),
           ),
           Container(
             margin: const EdgeInsets.all(5.0),
-            padding: const EdgeInsets.all(10.0),
-            decoration: BoxDecoration(
-              border: Border.all(
-                width: 5.0,
-                color: Colors.red,
+            child: Text(
+              teks4,
+              style: const TextStyle(
+                fontSize: 18.0,
               ),
-            ),
-            child: ElevatedButton(
-              child: const Text('Button 2'),
-              onPressed: () {},
-            ),
-          ),
-          Container(
-            color: Colors.tealAccent,
-            margin: const EdgeInsets.all(10.0),
-            padding: const EdgeInsets.all(3.0),
-            child: ElevatedButton(
-              child: const Text('Button 3'),
-              onPressed: () {},
+              textDirection: TextDirection.rtl,
+              textAlign: TextAlign.end,
             ),
           ),
         ],
