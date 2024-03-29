6a57120aed46332638b130352d7e571a0f8ca10d
suhairii <suhairi14000@gmail.com>
2024-03-29 10:04:44 +0700

    .

--- P07-Widget/widget_dasar/lib/main.dart
+++ P07-Widget/widget_dasar/lib/main.dart
@@ -1,88 +1,59 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
-
+// Muhammad Suhairi
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'TextAlign App',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
+      title: 'Profil dan Biodata',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Profil dan Biodata'),
+        ),
+        body: Center(
+          child: ProfilBiodata(),
+        ),
       ),
-      home: const Home(),
     );
   }
 }
 
-class Home extends StatelessWidget {
-  final String teks1 =
-      '''Nilai TextAlign.start pada teks dengan TextDirection.ltr''';
-  final String teks2 =
-      '''Nilai TextAlign.end pada teks dengan TextDirection.ltr''';
-  final String teks3 =
-      '''Nilai TextAlign.start pada teks dengan TextDirection.rtl''';
-  final String teks4 =
-      '''Nilai TextAlign.end pada teks dengan TextDirection.rtl''';
-  const Home({Key? key}) : super(key: key);
+class ProfilBiodata extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Metode Text.textAlign'),
-      ),
-      body: Column(
-        children: <Widget>[
-          Container(
-            margin: const EdgeInsets.all(10.0),
-            child: Text(
-              teks1,
-              style: const TextStyle(
-                fontSize: 18.0,
-              ),
-              textDirection: TextDirection.ltr,
-              textAlign: TextAlign.start,
-            ),
-          ),
-          Container(
-            margin: const EdgeInsets.all(10.0),
-            child: Text(
-              teks2,
-              style: const TextStyle(
-                fontSize: 18.0,
-              ),
-              textDirection: TextDirection.ltr,
-              textAlign: TextAlign.end,
-            ),
-          ),
-          Container(
-            margin: const EdgeInsets.all(10.0),
-            child: Text(
-              teks3,
-              style: const TextStyle(
-                fontSize: 18.0,
-              ),
-              textDirection: TextDirection.rtl,
-              textAlign: TextAlign.start,
-            ),
-          ),
-          Container(
-            margin: const EdgeInsets.all(5.0),
-            child: Text(
-              teks4,
-              style: const TextStyle(
-                fontSize: 18.0,
-              ),
-              textDirection: TextDirection.rtl,
-              textAlign: TextAlign.end,
-            ),
-          ),
-        ],
-      ),
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        CircleAvatar(
+          radius: 70,
+          backgroundImage: AssetImage(
+              'images/profile.jpg'), 
+        ),
+        SizedBox(height: 20),
+        Text(
+          'Nama Lengkap: Muhammad Suhairi',
+          style: TextStyle(fontSize: 20),
+        ),
+        Text(
+          'Tempat Tanggal Lahir: Teluk Lecah, 14 Januari 2004',
+          style: TextStyle(fontSize: 14),
+        ),
+        Text(
+          'Email: suhairi14000@gmail.com',
+          style: TextStyle(fontSize: 16),
+        ),
+        Text(
+          'Nomor HP: +62 82171300428',
+          style: TextStyle(fontSize: 16),
+        ),
+        Text(
+          'Hobi: Menulis',
+          style: TextStyle(fontSize: 16),
+        ),
+      ],
     );
   }
 }
